d92c07a6b0fb0784de037258b49082e9f3eabb98
taha khelifi <taha.khelifi@saintmichelannecy.fr>
2023-10-03 21:00:59 +0200

    1.4

--- Taha_Khelifi_projet_algo/MainWindow.xaml.cs
+++ Taha_Khelifi_projet_algo/MainWindow.xaml.cs
@@ -27,5 +27,36 @@ namespace Taha_Khelifi_projet_algo
         double N1 = 0;
         double N2 = 0;
         double Resultat = 0;
-    }
+
+        private void BTN_Compare_Click(object sender, RoutedEventArgs e)
+        {
+            // Convertir le texte des TextBox en nombres
+            if (double.TryParse(TB_1.Text, out N1) && double.TryParse(TB_2.Text, out N2))
+            {
+                // Comparer les nombres
+                if (N1 > N2)
+                {
+                    Resultat = N1;
+                }
+                else if (N1 < N2)
+                {
+                    Resultat = N2;
+                }
+                else
+                {
+                    // Si les nombres sont égaux
+                    TB_Resultat.Text = "Les nombres sont égaux";
+                    return; // Sortir de la méthode car nous avons déjà défini le texte dans TB_Resultat
+                }
+
+                // Afficher le résultat dans TB_Resultat
+                TB_Resultat.Text = Resultat.ToString();
+            }
+            else
+            {
+                // Gérer le cas où la conversion échoue
+                TB_Resultat.Text = "Erreur de saisie";
+            }
+        }
+    }  
 }
